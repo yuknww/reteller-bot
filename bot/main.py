d887367e8ef6024f3c72e9f9d5e24fef5ec67585
Тимофей Скачков <timoha425@icloud.com>
2026-03-16 19:34:14 +0300

    change wrong func

--- bot/main.py
+++ bot/main.py
@@ -33,7 +33,7 @@ dp = Dispatcher()
 # /retell week      — за неделю
 # /retell @username — сообщения конкретного пользователя за сутки
 
-@dp.message_handler(commands="how")
+@dp.message(commands="how")
 async def send_how(message: Message):
     await message.reply("Использование:\n"
                     "/retell 1 — за 1 час\n"
